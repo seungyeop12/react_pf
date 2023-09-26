f4c7b8ce6402d532a7900405ca247d67ff0020a5
seungyeop12 <art7449@gmail.com>
2023-09-26 14:43:47 +0900

    route 특정 url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Header from './components/common/header/Header';
+import { Route } from 'react-router-dom';
 import Department from './components/sub/department/Department';
+import Youtube from './components/sub/youtube/Youtube';
 import './styles/Global.scss';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Department />
+			<Route path='/department'>
+				<Department />
+			</Route>
+			<Route path='/youtube'>
+				<Youtube />
+			</Route>
 		</>
 	);
 }
