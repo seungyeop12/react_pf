4d36583cb3eaeda7589b064849a4188cf5737aed
seungyeop12 <art7449@gmail.com>
2023-09-26 15:13:48 +0900

    링크, 내부링크

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Route } from 'react-router-dom';
 import Department from './components/sub/department/Department';
 import Youtube from './components/sub/youtube/Youtube';
 import './styles/Global.scss';
+import Members from './components/sub/members/Members';
+import Gallery from './components/sub/gallery/Gallery';
+import Contact from './components/sub/contact/Contact';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Route path='/department'>
-				<Department />
-			</Route>
-			<Route path='/youtube'>
-				<Youtube />
-			</Route>
+			<Route path='/department' component={Department} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/members' component={Members} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/contact' component={Contact} />
 		</>
 	);
 }
---
+++ src/components/sub/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import Layout from '../../common/layout/Layout';
+// import styles from './Department.module.scss';
+// import clsx from 'clsx';
+
+export default function Contact() {
+	return (
+		<Layout title={'Contact'}>
+			<p>콘택트 페이지입니다.</p>
+		</Layout>
+	);
+}
