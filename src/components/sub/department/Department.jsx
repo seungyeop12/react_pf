cb74816a105d8dee3e7e99b79d17b4693551bf00
seungyeop12 <art7449@gmail.com>
2023-10-04 10:35:17 +0900

    useEffect로 외부 데이터 출력

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,9 +1,14 @@
 import Layout from '../../common/layout/Layout';
-// import styles from './Department.module.scss';
-// import clsx from 'clsx';
-import styles from './Department.module.scss';
+import { useEffect } from 'react';
 
 export default function Department() {
+	useEffect(() => {
+		fetch('/DB/department.json')
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json);
+			});
+	}, []);
 	return (
 		<Layout title={'Department'}>
 			<p>디파트먼트 페이지입니다.</p>
