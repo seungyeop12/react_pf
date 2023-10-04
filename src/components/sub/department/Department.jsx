4bb420e1178ea5ac1272445fa78ea0c34d5738de
seungyeop12 <art7449@gmail.com>
2023-10-04 11:08:53 +0900

    멤버 데이터 출력

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,17 +1,31 @@
 import Layout from '../../common/layout/Layout';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+const path = process.env.PUBLIC_URL;
 
 export default function Department() {
+	console.log('re-render');
+	const [Department, setDepartment] = useState([]);
 	useEffect(() => {
-		fetch('/DB/department.json')
+		fetch(`${path}/DB/department.json`)
 			.then((data) => data.json())
 			.then((json) => {
-				console.log(json);
+				console.log(json.members);
+				setDepartment(json.members);
 			});
 	}, []);
 	return (
 		<Layout title={'Department'}>
-			<p>디파트먼트 페이지입니다.</p>
+			{Department.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={`${path}/img/${member.pic}`} alt={member.name} />
+						</div>
+						<h2>{member.name}</h2>
+						<p>{member.position}</p>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
