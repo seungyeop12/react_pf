07b51155445668901c02986d68a0183b0a57e3cb
seungyeop12 <art7449@gmail.com>
2023-10-04 10:25:51 +0900

    members

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,6 +1,7 @@
 import Layout from '../../common/layout/Layout';
 // import styles from './Department.module.scss';
 // import clsx from 'clsx';
+import styles from './Department.module.scss';
 
 export default function Department() {
 	return (
